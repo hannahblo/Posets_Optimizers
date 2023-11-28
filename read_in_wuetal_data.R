f797f3b06eb81653055107c6f25f1df3f11d4056
Julian Rodemann <julian.rodemann@freenet.de>
2023-11-28 21:34:37 +0100

    Wu et al. data

---
+++ read_in_wuetal_data.R
@@ -0,0 +1,14 @@
+library(readr)
+library(tidyr)
+library(tidyverse)
+wuetal_data <- read_delim("DMOP_Wuetal_data.csv", 
+                               delim = ";", escape_double = FALSE, trim_ws = TRUE)
+#View(wuetal_data)
+
+wuetal_data = wuetal_data[,1:4]
+# remove standard deviation
+wuetal_data = wuetal_data %>% mutate(MIGD = trimws(str_replace(MIGD, "\\(.*?\\)", "")))
+wuetal_data = wuetal_data %>% mutate(MHVD = trimws(str_replace(MHVD, "\\(.*?\\)", "")))
+wuetal_data
+
+save(wuetal_data, file = "DMOP_Wuetal_data_no_std_err.csv")
