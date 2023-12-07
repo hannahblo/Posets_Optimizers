93cbd9778f010f47f837c9e6f34b82f678bcee52
Hannah Blocher <H.Blocher@campus.lmu.de>
2023-12-07 07:53:45 +0100

    cleaning the repository

---
+++ _setup_session.R
@@ -0,0 +1,17 @@
+
+install.packages("reshape")
+install.packages("dplyr")
+install.packages("data.table")
+install.packages("ggplot2")
+install.packages("forcat")
+install.packages("gridExtra")
+install.packages("stargazer")
+install.packages("prefmod")
+install.packages("reshape2")
+install.packages("utils")
+
+install.packages("BiocManager")
+BiocManager::install("hasseDiagram")
+
+install.packages("devtools")
+devtools::install_github("hannahblo/ddandrda")
